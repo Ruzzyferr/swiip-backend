01e338f0bcc2b2dab7c3eeb75b1000662019f9b9
ruzzyfer <bulutruzgaremir@gmail.com>
2026-01-03 19:02:27 +0300

    DB fix

--- prisma.config.ts
+++ prisma.config.ts
@@ -9,6 +9,7 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    // Prefer DIRECT_URL for migrations if available to bypass connection pooler
+    url: process.env["DIRECT_URL"] ?? process.env["DATABASE_URL"],
   },
 });
